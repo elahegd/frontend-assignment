2cbf85b4ea2c4d2807354c152cd96b96bcc0f82a
egoye3 <elahe.goodarzi@scania.com>
2024-11-13 12:54:55 +0100

    feat: add Album component and display its data on it

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+import data from "./data/albums.json";
+import Album from "./components/Album.vue";
+
+const albums = computed(() => {
+  return data.albums.sort((a, b) => b.year - a.year);
+});
+</script>
 
 <template>
   <div
-    class="h-screen w-full flex bg-gradient-to-br from-teal-500 to-slate-700"
+    class="h-screen w-full flex flex-col gap-3 bg-gradient-to-br from-teal-500 to-slate-700 overflow-auto p-5"
   >
-    <div class="mx-auto my-auto text-white">
-      <img
-        src="https://brightbid.com/wp-content/uploads/2023/06/logo.svg"
-        alt=""
-        class="w-80 mx-auto"
-      />
-      <h1 class="mx-auto text-center text-3xl font-bold mt-4">
-        Hello and welcome 👋
-      </h1>
-      <h1 class="mx-auto text-center text-3xl font-bold">Good luck!</h1>
-      <h1 class="mx-auto text-center text-3xl font-bold">./App.vue</h1>
+    <h1 class="text-3xl font-bold text-white">Albums</h1>
+
+    <div
+      v-if="albums"
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-3"
+    >
+      <div v-for="(album, index) in albums" :key="index" class="cursor-pointer">
+        <Album :albumItem="album" />
+      </div>
     </div>
   </div>
 </template>
---
+++ src/components/Album.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  albumItem: {
+    name: String,
+    year: Number,
+    artist: String,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="flex items-center gap-2 bg-white text-black p-4 rounded-lg shadow-lg transform transition duration-200 hover:scale-105 cursor-pointer"
+  >
+    <div class="flex flex-col w-2/3">
+      <h2 class="text-lg font-semibold truncate">{{ albumItem.name }}</h2>
+      <p class="text-sm text-grey-500">
+        {{ albumItem.year }} - {{ albumItem.artist }}
+      </p>
+    </div>
+  </div>
+</template>
