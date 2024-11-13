7d5bb0955715b14398d984dde0d5edb8e6f27996
egoye3 <elahe.goodarzi@scania.com>
2024-11-13 13:06:15 +0100

    feat: adding cover image in album data and album component

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -4,6 +4,7 @@ defineProps({
     name: String,
     year: Number,
     artist: String,
+    cover: String | null,
   },
 });
 </script>
@@ -12,6 +13,15 @@ defineProps({
   <div
     class="flex items-center gap-2 bg-white text-black p-4 rounded-lg shadow-lg transform transition duration-200 hover:scale-105 cursor-pointer"
   >
+    <img
+      class="rounded-full bg-gray-50 w-1/3"
+      :src="
+        albumItem.cover !== null
+          ? albumItem.cover
+          : 'https://placehold.co/300x300/png'
+      "
+      :alt="albumItem.name"
+    />
     <div class="flex flex-col w-2/3">
       <h2 class="text-lg font-semibold truncate">{{ albumItem.name }}</h2>
       <p class="text-sm text-grey-500">
